95a2ed92ebf7a16058f3dbfde0cd370854c63ad4
Mateusz Myrcik <mateuszmyrcik@MacBook-Pro-Mateusz.local>
2022-09-07 21:04:09 +0200

    add: add cookie banner

--- src/ui/master-layout/index.tsx
+++ src/ui/master-layout/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import CookieConsent from "react-cookie-consent";
 import { NavigationItems } from "../../configs/navigation";
 import { FooterComponent } from "../footer";
 
@@ -24,6 +25,16 @@ export const MasterLayoutComponent: React.FC<IMasterLayoutComponent> = ({
       </header>
       <main className="min-h-screen" style={{ paddingTop: "72px" }}>
         {children}
+        <CookieConsent
+          location="bottom"
+          buttonText="Accept"
+          cookieName="myAwesomeCookieName2"
+          style={{ background: "#2B373B" }}
+          buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+          expires={150}
+        >
+          This website uses cookies to enhance the user experience
+        </CookieConsent>
       </main>
       <footer>
         <FooterComponent />
